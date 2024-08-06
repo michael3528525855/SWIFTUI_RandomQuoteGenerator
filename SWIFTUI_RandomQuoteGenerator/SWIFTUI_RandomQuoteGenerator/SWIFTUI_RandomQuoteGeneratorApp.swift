dde47d80c3efc875fdea798083ced9058af5133e
michael halim <153228492+michael3528525855@users.noreply.github.com>
2024-08-06 23:54:47 +0300

    Add files via upload

---
+++ SWIFTUI_RandomQuoteGenerator/SWIFTUI_RandomQuoteGenerator/SWIFTUI_RandomQuoteGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_RandomQuoteGeneratorApp.swift
+//  SWIFTUI_RandomQuoteGenerator
+//
+//  Created by michael on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_RandomQuoteGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
